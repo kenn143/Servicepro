af1546b7a290acfdedd539b08858d07192075965
Kenneth <sumantingk7@gmail.com>
2025-10-02 14:22:08 +0800

    Addtional condition

--- src/pages/QuotationList.tsx
+++ src/pages/QuotationList.tsx
@@ -395,19 +395,26 @@ useEffect(() => {
 
                           </td>
                           <td className="px-3 sm:px-4 py-2">
-                            {item.status === "Approved" ? (
-                              <div className=" text-black px-1 py-1 rounded cursor-default w-15 dark:text-white">
+                          {item.status === "Approved" ? (
+                              <div className="text-black px-1 py-1 rounded cursor-default w-15 dark:text-white">
                                 <span className="inline-flex items-center gap-1 text-green-600">
-                            <span className="w-3 h-3 rounded-full bg-green-600"></span> Approved
-                          </span>
+                                  <span className="w-3 h-3 rounded-full bg-green-600"></span> Approved
+                                </span>
+                              </div>
+                            ) : item.status === "Waiting for Approval" ? (
+                              <div className="text-black px-1 py-1 rounded cursor-default w-15 dark:text-white">
+                                <span className="inline-flex items-center gap-1 text-blue-500 leading-none">
+                                  <span className="w-6 h-3 rounded-full bg-blue-500"></span> Waiting for Approval
+                                </span>
                               </div>
                             ) : (
-                              <div className=" text-blue-700 px-1 py-1 rounded cursor-default w-15 dark:text-white">
-                               <span className="inline-flex items-center gap-1 text-yellow-600 ">
-                            <span className="w-3 h-3 rounded-full bg-yellow-600"></span> Pending
-                          </span>
+                              <div className="text-blue-700 px-1 py-1 rounded cursor-default w-15 dark:text-white">
+                                <span className="inline-flex items-center gap-1 text-yellow-600">
+                                  <span className="w-3 h-3 rounded-full bg-yellow-600"></span> Pending
+                                </span>
                               </div>
                             )}
+
                           </td>
                           <td className="px-3 sm:px-4 py-2">
                             <button
