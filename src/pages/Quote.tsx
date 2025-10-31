90ebfe48a463ae15b25df94d227deb6badce6079
Kenneth <sumantingk7@gmail.com>
2025-10-31 23:58:58 +0800

    change recordId to userId

--- src/pages/Quote.tsx
+++ src/pages/Quote.tsx
@@ -146,7 +146,7 @@ useEffect(() => {
       jobTitle: jobTitle,
       ClientId: selected,
       ClientMessage: clientMessage,
-      recordId: getToken()?.ID
+      userId: getToken()?.ID
     };
 
 console.log("finalquote",finalQuote)
