df3d59e9bceda101d4f08b9034a5b5fc5ea4ff9a
Kenneth <sumantingk7@gmail.com>
2025-11-10 17:24:26 +0800

    recommit for quote saving

--- src/pages/Quote.tsx
+++ src/pages/Quote.tsx
@@ -75,13 +75,13 @@ useEffect(() => {
     setJobTitle(e.target.value);
   };
 
-  const toBase64 = (file: File): Promise<string> =>
-    new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => resolve(reader.result as string);
-      reader.onerror = (error) => reject(error);
-    });
+  // const toBase64 = (file: File): Promise<string> =>
+  //   new Promise((resolve, reject) => {
+  //     const reader = new FileReader();
+  //     reader.readAsDataURL(file);
+  //     reader.onload = () => resolve(reader.result as string);
+  //     reader.onerror = (error) => reject(error);
+  //   });
 
   const addItem = async () => {
     const cloudName = "doj0vye62";
